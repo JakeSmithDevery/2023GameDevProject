dd6d00850aaa2f52b01aa2c8d6157c365c8594c2
Jake Smith <123363883+Tohru1357@users.noreply.github.com>
2023-04-19 15:00:05 +0100

    Added necessary scripts

---
+++ Assets/Scripts/EnemyHealth.cs
@@ -0,0 +1,28 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyHealth : ObjectHealth
+{
+    public GameObject ZombieRemains;
+    public GameObject ZombieExplosion;
+
+
+
+    public override void HandleCollision(GameObject otherObject)
+    {
+        if (otherObject.CompareTag("Bullet"))
+        {
+            Bullet b = otherObject.GetComponent<Bullet>();
+            SubtractHealth(b.Damage);
+        }
+        base.HandleCollision(otherObject);
+    }
+
+    public override void OnDeath()
+    {
+        Instantiate(ZombieRemains, transform.position, Quaternion.identity);
+        Destroy(gameObject);
+        base.OnDeath();
+    }
+}
